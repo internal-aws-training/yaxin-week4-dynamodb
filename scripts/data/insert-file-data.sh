d4b095fb9ca09a82511257585a1170a228aedaf3
sunnyaxin <137126846@qq.com>
2020-06-04 09:33:38 +0800

    Update code.

--- scripts/data/insert-file-data.sh
+++ scripts/data/insert-file-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname ${BASH_SOURCE[0]})/..
+cd $(dirname ${BASH_SOURCE[0]})/../..
 
 aws dynamodb batch-write-item \
     --request-items file://json/insertData.json
--- scripts/table/clean-table.sh
+++ scripts/table/clean-table.sh
@@ -3,4 +3,3 @@
 cd $(dirname ${BASH_SOURCE[0]})/..
 
 aws dynamodb delete-table --table-name Project_Yaxin
-            
